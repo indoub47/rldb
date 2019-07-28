7979012bdeaf195ab6bab36aaa7619fb12297d3f
indoub47 <ev.ilginis@gmail.com>
2019-07-28 14:41:06 +0300

    minor editions for deploying

---
+++ client/build/precache-manifest.a891835917ea9fe2809d956eb3733b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e94b5b90441531d06c6986acf24107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a68cc34458d8efa39f8",
+    "url": "/static/css/main.4b72b5a0.chunk.css"
+  },
+  {
+    "revision": "44b6fb549584f71bcfb1",
+    "url": "/static/js/2.5b73044f.chunk.js"
+  },
+  {
+    "revision": "7a68cc34458d8efa39f8",
+    "url": "/static/js/main.e5272704.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c45fbef2aa2605c19a0db9161046400",
+    "url": "/static/media/showcase.2c45fbef.jpg"
+  }
+]);
