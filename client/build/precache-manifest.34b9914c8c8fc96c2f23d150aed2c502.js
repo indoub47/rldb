d906acace8aa03c2521bea09c6997751e5c9b06f
indoub47 <ev.ilginis@gmail.com>
2019-07-29 21:17:32 +0300

    added build

---
+++ client/build/precache-manifest.34b9914c8c8fc96c2f23d150aed2c502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af819ff419421db927c68d52f6276e2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bbd3aff63763321bf88",
+    "url": "/static/css/main.4b72b5a0.chunk.css"
+  },
+  {
+    "revision": "44b6fb549584f71bcfb1",
+    "url": "/static/js/2.5b73044f.chunk.js"
+  },
+  {
+    "revision": "5bbd3aff63763321bf88",
+    "url": "/static/js/main.d62aef89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c45fbef2aa2605c19a0db9161046400",
+    "url": "/static/media/showcase.2c45fbef.jpg"
+  }
+]);
